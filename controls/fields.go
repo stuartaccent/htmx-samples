b213d7dcad33d0306eb28022956837b8c7e351eb
Stuart George <stuart@accentdesign.co.uk>
2024-03-09 22:17:51 +0000

    multi choice field

--- controls/fields.go
+++ controls/fields.go
@@ -8,3 +8,8 @@ type ChoiceField struct {
 	Value   string `form:"value" binding:"required"`
 	Choices []string
 }
+
+type MultiChoiceField struct {
+	Values  []string `form:"values" binding:"required,min=1"`
+	Choices []string
+}
--- main.go
+++ main.go
@@ -17,13 +17,19 @@ var (
 	staticFS embed.FS
 	//go:embed templates/*
 	templatesFS embed.FS
-	// templates
 )
 
 func main() {
 	g := gin.Default()
 
-	tmpls := template.Must(template.ParseFS(templatesFS, "templates/*.gohtml"))
+	funcMap := template.FuncMap{
+		"stringInSlice": stringInSlice,
+	}
+
+	tmpls, err := template.New("").Funcs(funcMap).ParseFS(templatesFS, "templates/*.gohtml")
+	if err != nil {
+		log.Fatalf("Unable to parse templates: %v", err)
+	}
 	g.SetHTMLTemplate(tmpls)
 
 	static, err := fs.Sub(staticFS, "static")
@@ -39,6 +45,8 @@ func main() {
 	g.POST("/textarea", TextareaHandler)
 	g.GET("/choice", ChoiceHandler)
 	g.POST("/choice", ChoiceHandler)
+	g.GET("/multi-choice", MultiChoiceHandler)
+	g.POST("/multi-choice", MultiChoiceHandler)
 
 	log.Print("Listening...")
 	http.ListenAndServe(":80", g)
@@ -49,13 +57,12 @@ func indexHandler(c *gin.Context) {
 }
 
 func InputHandler(c *gin.Context) {
-	field := controls.TextField{
-		Value: "Lorem ipsum dolor sit amet",
-	}
-	var control controls.Control = &controls.FormControl[controls.TextField]{
-		URL:          c.Request.URL.Path,
-		Label:        "Input",
-		Field:        field,
+	control := &controls.FormControl[controls.TextField]{
+		URL:   c.Request.URL.Path,
+		Label: "Input",
+		Field: controls.TextField{
+			Value: "Lorem ipsum dolor sit amet",
+		},
 		ReadTemplate: "inputRead",
 		EditTemplate: "inputEdit",
 		SaveFunc: func(tx *controls.FormControl[controls.TextField]) error {
@@ -63,17 +70,19 @@ func InputHandler(c *gin.Context) {
 			return nil
 		},
 	}
+	if c.Request.Method == http.MethodPost {
+		control.Field.Value = ""
+	}
 	control.GinHandler(c)
 }
 
 func TextareaHandler(c *gin.Context) {
-	field := controls.TextField{
-		Value: "Lorem ipsum dolor sit amet, eum eligendi petentium temporibus te, et erant volumus erroribus duo. Id duo choro nullam philosophia.",
-	}
-	var control controls.Control = &controls.FormControl[controls.TextField]{
-		URL:          c.Request.URL.Path,
-		Label:        "Textarea",
-		Field:        field,
+	control := &controls.FormControl[controls.TextField]{
+		URL:   c.Request.URL.Path,
+		Label: "Textarea",
+		Field: controls.TextField{
+			Value: "Lorem ipsum dolor sit amet, eum eligendi petentium temporibus te, et erant volumus erroribus duo. Id duo choro nullam philosophia.",
+		},
 		ReadTemplate: "textareaRead",
 		EditTemplate: "textareaEdit",
 		SaveFunc: func(tx *controls.FormControl[controls.TextField]) error {
@@ -81,18 +90,20 @@ func TextareaHandler(c *gin.Context) {
 			return nil
 		},
 	}
+	if c.Request.Method == http.MethodPost {
+		control.Field.Value = ""
+	}
 	control.GinHandler(c)
 }
 
 func ChoiceHandler(c *gin.Context) {
-	field := controls.ChoiceField{
-		Value:   "Option 1",
-		Choices: []string{"Option 1", "Option 2", "Option 3", "Option 4", "Option 5"},
-	}
-	var control controls.Control = &controls.FormControl[controls.ChoiceField]{
-		URL:          c.Request.URL.Path,
-		Label:        "Choice",
-		Field:        field,
+	control := &controls.FormControl[controls.ChoiceField]{
+		URL:   c.Request.URL.Path,
+		Label: "Choice",
+		Field: controls.ChoiceField{
+			Value:   "Option 1",
+			Choices: []string{"Option 1", "Option 2", "Option 3", "Option 4", "Option 5"},
+		},
 		ReadTemplate: "choiceRead",
 		EditTemplate: "choiceEdit",
 		SaveFunc: func(tx *controls.FormControl[controls.ChoiceField]) error {
@@ -100,5 +111,38 @@ func ChoiceHandler(c *gin.Context) {
 			return nil
 		},
 	}
+	if c.Request.Method == http.MethodPost {
+		control.Field.Value = ""
+	}
 	control.GinHandler(c)
+}
+
+func MultiChoiceHandler(c *gin.Context) {
+	control := &controls.FormControl[controls.MultiChoiceField]{
+		URL:   c.Request.URL.Path,
+		Label: "Multi Choice",
+		Field: controls.MultiChoiceField{
+			Values:  []string{"Option 1", "Option 2"},
+			Choices: []string{"Option 1", "Option 2", "Option 3", "Option 4", "Option 5"},
+		},
+		ReadTemplate: "multiChoiceRead",
+		EditTemplate: "multiChoiceEdit",
+		SaveFunc: func(tx *controls.FormControl[controls.MultiChoiceField]) error {
+			log.Printf("Saving: %s", tx.Field.Values)
+			return nil
+		},
+	}
+	if c.Request.Method == http.MethodPost {
+		control.Field.Values = []string{}
+	}
+	control.GinHandler(c)
+}
+
+func stringInSlice(str string, list []string) bool {
+	for _, v := range list {
+		if v == str {
+			return true
+		}
+	}
+	return false
 }
